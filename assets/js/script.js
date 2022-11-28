f18530aefcc9def26e1a6ef0a848affd21aa87be
PMengler <114950259+PMengler@users.noreply.github.com>
2022-11-28 13:54:52 -0800

    lots of js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,93 +1,210 @@
-var cityList = document.querySelector('ul');
-var dailyWeather = document.querySelector('.card');
-var fetchButton = document.getElementById('fetch-button');
-var APIKey = '394144fc80ac73b599b4202e469c4af9';
-var city;
-var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={394144fc80ac73b599b4202e469c4af9}&per_page=5';
+// var cityList = document.querySelector('ul');
+// var dailyWeather = document.querySelector('.card');
+// var fetchButton = document.getElementById('fetch-button');
+var searchBtn = $('#search-button');
+var clearBtn = $('#clear-button');
+var APIkey = '394144fc80ac73b599b4202e469c4af9';
+var cityInputEl = $('city-name');
+var currentCity;
 
 // Autocomplete for US Capital cities
 $(function () {
-    var cityNames = [
-      'Montgomery',
-      'Juneau',
-      'Phoenix',
-      'Little Rock',
-      'Sacramento',
-      'Denver',
-      'Hartford',
-      'Dover',
-      'Tallahassee',
-      'Atlanta',
-      'Honolulu',
-      'Boise',
-      'Springfield',
-      'Indianapolis',
-      'Des Moines',
-      'Topeka',
-      'Frankfort',
-      'Baton Rouge',
-      'Augusta',
-      'Annapolis',
-      'Boston',
-      'Lansing',
-      'St. Paul',
-      'Jackson',
-      'Jefferson City',
-      'Helena',
-      'Lincoln',
-      'Carson City',
-      'Concord',
-      'Trenton',
-      'Santa Fe',
-      'Albany',
-      'Raleigh',
-      'Bismarck',
-      'Columbus',
-      'Oklahoma City',
-      'Salem',
-      'Harrisburg',
-      'Providence',
-      'Columbia',
-      'Pierre',
-      'Nashville',
-      'Austin',
-      'Salt Lake City',
-      'Montpellier',
-      'Richmond',
-      'Olympia',
-      'Charleston',
-      'Madison',
-      'Cheyenne',
-    ];
-    $('#city-name').autocomplete({
-      source: cityNames,
-    });
+  var capitalCityNames = [
+    'Montgomery',
+    'Juneau',
+    'Phoenix',
+    'Little Rock',
+    'Sacramento',
+    'Denver',
+    'Hartford',
+    'Dover',
+    'Tallahassee',
+    'Atlanta',
+    'Honolulu',
+    'Boise',
+    'Springfield',
+    'Indianapolis',
+    'Des Moines',
+    'Topeka',
+    'Frankfort',
+    'Baton Rouge',
+    'Augusta',
+    'Annapolis',
+    'Boston',
+    'Lansing',
+    'St. Paul',
+    'Jackson',
+    'Jefferson City',
+    'Helena',
+    'Lincoln',
+    'Carson City',
+    'Concord',
+    'Trenton',
+    'Santa Fe',
+    'Albany',
+    'Raleigh',
+    'Bismarck',
+    'Columbus',
+    'Oklahoma City',
+    'Salem',
+    'Harrisburg',
+    'Providence',
+    'Columbia',
+    'Pierre',
+    'Nashville',
+    'Austin',
+    'Salt Lake City',
+    'Montpellier',
+    'Richmond',
+    'Olympia',
+    'Charleston',
+    'Madison',
+    'Cheyenne',
+  ];
+  $('#city-name').autocomplete({
+    source: capitalCityNames,
   });
+});
 
 //getApi function is called when the fetchButton is clicked
 
-function getApi() {
+function getApi(data) {
+  
+  var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lon}&exclude=minutely,hourly,alerts&units=imperial&appid=${APIkey}`;
+  fetch(requestUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      
+      // Create Current Weather card
+      var currentWeatherEl = $('#current-weather');
+      currentWeatherEl.addClass('card text-dark bg-light mb-3');
+
+      // Current City name in current weather card
+      var cityNameEl = $('<h3>');
+      cityNameEl.text(currentCity);
+      currentWeatherEl.append(cityNameEl);
+
+      // Current date
+      var currentCityDate = data.current.dt;
+      currentCityDate = dayjs().unix(currentCityDate).format('MM/DD/YYYY');
+      var currentDateEl = $('<span>');
+      currentDateEl.text(' (${currentCityDate}) ');
+      cityNameEl.append(currentDateEl);
+
+      // Add weather emoji for weather in current weather card
+      var currentWeatherEmoji =  data.current.weather[0].icon;
+      var currentWeatherEmojiEl = $('<img>');
+      currentWeatherEmojiEl.attr('src', "http://openweathermap.org/img/wn/" + currentWeatherEmoji + ".png");
+      cityWeatherEl.append(currentWeatherEmojiEl);
+
+      // temperature
+      var currentTemp = data.current.temp;
+      var currentTempEl = $('<p>');
+      currentTempEl.text(`Temp: ${currentTemp}°F`)
+      currentWeatherEl.append(currentTempEl);
+
+      // Humidity
+      var currentHumidity = data.current.humidity;
+      var currentHumidityEl = $('<p>');
+      currentHumidityEl.text(`Humidity: ${currentHumidity}%`)
+      currentWeatherEl.append(currentHumidityEl);
+
+      // UV
+      var currentUv = data.current.uvi;
+      var currentUvEl = $('<p>');
+      currentUvEl.text(`UV: ${currentUv}`)
+      currentWeatherEl.append(currentUvEl);
+
+      // 5 - Day Forecast
+      // create 5 Day Forecast <h2> card headers
+      var fiveDayForecastHeaderEl = $('#forecastHeader');
+      var fiveDayHeaderEl = $('<h3>');
+      fiveDayHeaderEl.text('5-Day Forecast:');
+      fiveDayForecastHeaderEl.append(fiveDayHeaderEl);
+
+      var fiveDayForecastEl = $('#forecast');
+
+      // get key weather info from API data for five day forecast and display
+      for (var i = 1; i <=5; i++) {
+          var date;
+          var temp;
+          var icon;
+          var wind;
+          var humidity;
+
+          date = data.daily[i].dt;
+          date = dayjs().unix(date).format("MM/DD/YYYY");
+
+          temp = data.daily[i].temp.day;
+          icon = data.daily[i].weather[0].icon;
+          wind = data.daily[i].wind_speed;
+          humidity = data.daily[i].humidity;
+
+          // create a card
+          var card = document.createElement('div');
+          card.classList.add('card', 'row-3', 'm-1', 'bg-info', 'text-white');
+          
+          // create card body and append
+          var cardBody = document.createElement('div');
+          cardBody.classList.add('card-body');
+          cardBody.innerHTML = `<h6>${date}</h6>
+                                <img src= "http://openweathermap.org/img/wn/${icon}.png"> </><br>
+                                  ${temp}°F<br>
+                                  ${wind} mph <br>
+                                  ${humidity}%`
+          
+          card.appendChild(cardBody);
+          fiveDayForecastEl.append(card);
+        }
+    })
+  return;
+};
+
+// get city coordinates from Open Weather current weather data and send to One Call API
+function getCoordinates () {
+  var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${APIkey}`;
+  var storedCities = JSON.parse(localStorage.getItem("cities")) || [];
 
   fetch(requestUrl)
     .then(function (response) {
-      console.log(response.status)
-      return response.json().data;
+      if (response.status >= 200 && response.status <= 299) {
+          return response.json();
+        } else {
+          throw Error(response.statusText);
+        }
+    })
+    .then(function(data) {
+
+      var cityInfo = {
+          city: currentCity,
+          lon: data.coord.lon,
+          lat: data.coord.lat
+      }
+
+      storedCities.push(cityInfo);
+      localStorage.setItem("cities", JSON.stringify(storedCities));
+
+      // displaySearchHistory();
+
+      return cityInfo;
     })
     .then(function (data) {
-      //looping over the fetch response and inserting the URL of your repos into a list
-      for (var i = 0; i < 5; i++) {
-        console.log(response.data)
-        //Create a list element
-        var listItem = document.createElement('li'); // should have list elements created for cities that i've looked up
-        //and cards created for the daily weather in each city
+      getApi(data);
+    })
+    return;
+};
 
-        //Set the text of the list element to the JSON response's .html_url property
-        // listItem.textContent = data[i].html_url;
+function submitSearch (event) {
+  event.preventDefault();
+  currentCity = cityInputEl.val();
 
-        //Append the li element to the id associated with the ul element.
-        // repoList.appendChild(listItem);
-      }
-    });
+  // clearCurrentCityWeather();
+  getCoordinates();
+
+  return;
 }
 
-fetchButton.addEventListener('click', getApi);
+searchBtn.on('click', submitSearch);
